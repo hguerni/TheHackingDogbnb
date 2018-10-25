4fccf2cc8cc7e93235e20fd2117860112f10b9f6
hguerni <hava3@hotmail.fr>
2018-10-25 19:05:55 +0200

    SEEDED

--- TheHackingDogbnb/db/seeds.rb
+++ TheHackingDogbnb/db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  City.create!(name: Faker::RickAndMorty.location)
+end
+10.times do
+  Dogsitter.create!(first_name: Faker::Fallout.character, last_name: Faker::GameOfThrones.house, city_id: rand(City.first.id..City.last.id))
+end
+
+10.times do
+  Dog.create!(name: Faker::Book.genre, city_id: rand(City.first.id..City.last.id))
+end
+
+10.times do
+  Stroll.create!(dog_id: rand(Dog.first.id..Dog.last.id),dogsitter_id: rand(Dogsitter.first.id..Dogsitter.last.id), city_id: rand(City.first.id..City.last.id))
+end
